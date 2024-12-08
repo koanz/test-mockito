cf5107a69d7a8a3a705346cedd735adfb617e953
Cristian <cristiananzawa@gmail.com>
2024-12-08 19:18:15 -0300

    feats: findByName and findByNameWithQuestions in services

--- src/main/java/org/koshiroanz/appmockito/ejemplo/services/ExamServiceImpl.java
+++ src/main/java/org/koshiroanz/appmockito/ejemplo/services/ExamServiceImpl.java
@@ -2,26 +2,36 @@ package org.koshiroanz.appmockito.ejemplo.services;
 
 import org.koshiroanz.appmockito.ejemplo.models.Exam;
 import org.koshiroanz.appmockito.ejemplo.repositories.IExamRepository;
+import org.koshiroanz.appmockito.ejemplo.repositories.IQuestionRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class ExamServiceImpl implements IExamService {
     private IExamRepository repository;
+    private IQuestionRepository questionRepository;
 
-    public ExamServiceImpl(IExamRepository repository) {
+    public ExamServiceImpl(IExamRepository repository, IQuestionRepository questionRepository) {
         this.repository = repository;
+        this.questionRepository = questionRepository;
     }
 
     @Override
-    public Exam findByName(String name) {
-        Optional<Exam> examOpt = repository.findAll()
+    public Optional<Exam> findByName(String name) {
+        return repository.findAll()
                 .stream()
                 .filter(e -> e.getName().contains(name))
                 .findFirst();
+    }
 
+    @Override
+    public Exam findByNameWithQuestions(String name) {
+        Optional<Exam> examOpt = this.findByName(name);
         Exam exam = null;
         if(examOpt.isPresent()) {
             exam = examOpt.orElseThrow();
+            List<String> questions = questionRepository.findQuestionByExamId(exam.getId());
+            exam.setQuestions(questions);
         }
 
         return exam;
--- src/main/java/org/koshiroanz/appmockito/ejemplo/services/IExamService.java
+++ src/main/java/org/koshiroanz/appmockito/ejemplo/services/IExamService.java
@@ -2,6 +2,9 @@ package org.koshiroanz.appmockito.ejemplo.services;
 
 import org.koshiroanz.appmockito.ejemplo.models.Exam;
 
+import java.util.Optional;
+
 public interface IExamService {
-    Exam findByName(String name);
+    Optional<Exam> findByName(String name);
+    Exam findByNameWithQuestions(String name);
 }
